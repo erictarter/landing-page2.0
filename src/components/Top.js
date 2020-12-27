927c63693cb94c25e9942f3ac9c8cb88ed0563a2
Eric Tarter <etarter00@gmail.com>
2020-12-26 21:50:23 -0800

    final

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, animateScroll as scroll } from 'react-scroll';
 
 export const Top = () => {
+  setTimeout(() => {
+    document.querySelector('.message1').classList.add('show');
+  }, 200);
+  setTimeout(() => {
+    document.querySelector('.message2').classList.add('show');
+  }, 800);
+  setTimeout(() => {
+    document.querySelector('.message3').classList.add('show');
+  }, 1400);
+  setTimeout(() => {
+    document.querySelector('.message4').classList.add('show');
+  }, 2000);
+  setTimeout(() => {
+    document.querySelector('.message1').classList.remove('show');
+    document.querySelector('.message2').classList.remove('show');
+    document.querySelector('.message3').classList.remove('show');
+    document.querySelector('.message4').classList.remove('show');
+    document.querySelector('.ss-links').classList.add('show');
+  }, 3000);
+
   return (
     <div>
       <div className='header'>
+        <div className='message'>
+          <span className='message1'>Welcome</span>
+          <br />
+          <span className='message2'>To</span> <br />
+          <span className='message3'>My</span> <br />
+          <span className='message4'>Page</span>
+        </div>
         <div className='mobile-overlay'></div>
         <div className='avatar-2'></div>
         <div className='ss-links'>
